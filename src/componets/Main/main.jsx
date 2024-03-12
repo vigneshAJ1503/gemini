2b0e045b0f7c6f20036dde10ea5ebc2c0904feeb
vigneshAJ1503 <vigneshaj403@gmail.com>
2024-03-12 09:48:40 +0530

    test

--- src/componets/Main/main.jsx
+++ src/componets/Main/main.jsx
@@ -18,30 +18,30 @@ const main = () => {
             <div className="main-container">
 
 
-                {!showResult?<>
-                        <div className="greet">
-                            <p><span>Hello, User...</span></p>
-                            <p>How Can I Help You Today?</p>
+                {!showResult ? <>
+                    <div className="greet">
+                        <p><span>Hello, User...</span></p>
+                        <p>How Can I Help You Today?</p>
+                    </div>
+                    <div className="cards">
+                        <div className="card">
+                            <p>Suggest beautiful places to see on an upcoming road trip</p>
+                            <img src={assets.compass_icon} alt="" />
                         </div>
-                        <div className="cards">
-                            <div className="card">
-                                <p>Suggest beautiful places to see on an upcoming road trip</p>
-                                <img src={assets.compass_icon} alt="" />
-                            </div>
-                            <div className="card">
-                                <p>Briefly summarize this concept: urban planning</p>
-                                <img src={assets.bulb_icon} alt="" />
-                            </div>
-                            <div className="card">
-                                <p>Brainstorm team bonding activites for our work retreat</p>
-                                <img src={assets.message_icon} alt="" />
-                            </div>
-                            <div className="card">
-                                <p>Improve the readability of the following code</p>
-                                <img src={assets.code_icon} alt="" />
-                            </div>
+                        <div className="card">
+                            <p>Briefly summarize this concept: urban planning</p>
+                            <img src={assets.bulb_icon} alt="" />
                         </div>
-                    </>
+                        <div className="card">
+                            <p>Brainstorm team bonding activites for our work retreat</p>
+                            <img src={assets.message_icon} alt="" />
+                        </div>
+                        <div className="card">
+                            <p>Improve the readability of the following code</p>
+                            <img src={assets.code_icon} alt="" />
+                        </div>
+                    </div>
+                </>
                     : <div className='result'>
                         <div className="result-title">
                             <img src={assets.user_icon} alt="" />
@@ -50,26 +50,30 @@ const main = () => {
                         <div className="result-data">
                             <img src={assets.gemini_icon} alt="" />
                             {loading
-                            ? <div className='loader'>
-                                <hr />
-                                <hr />
-                                <hr />
-                               </div> 
-                               :
-                               <p dangerouslySetInnerHTML={{__html:resultData}}></p>
-                        }
-                            
+                                ? <div className='loader'>
+                                    <hr />
+                                    <hr />
+                                    <hr />
+                                </div>
+                                :
+                                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                            }
+
                         </div>
                     </div>
                 }
 
                 <div className="main-bottom">
                     <div className="search-box">
-                        <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Enter the prompt' />
+                        <input onKeyDown={(e) => {
+                            if(e.key == 'Enter'){
+                                onSent();
+                            }
+                        }} onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Enter the prompt' />
                         <div>
                             <img src={assets.gallery_icon} alt="" />
                             <img src={assets.mic_icon} alt="" />
-                            {input?<img onClick={() => onSent()} src={assets.send_icon} alt="" />:null}
+                            {input ? <img onClick={() => onSent()} src={assets.send_icon} alt="" /> : null}
                         </div>
                     </div>
                     <p className="bottom-info">Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps</p>
@@ -78,5 +82,4 @@ const main = () => {
         </div>
     )
 }
-
 export default main
